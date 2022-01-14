68a0b930d23de66faf80a1f3db79e028bda3092a
PaulHaze <dev@artemist.com.au>
2022-01-15 08:41:03 +1100

    add router and basic route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter } from 'react-router-dom';
+
 //* ADD THESE FOR REDUX
 // import { Provider } from 'react-redux';
 // import store from './redux/store';
@@ -12,9 +14,11 @@ import { AppRoutes } from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <AppRoutes />
-    {/* </Provider> */}
+    <BrowserRouter>
+      {/* <Provider store={store}> */}
+      <AppRoutes />
+      {/* </Provider> */}
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../views';
 
 export function AppRoutes() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
