e87c42df2a1f2b99abeeadc517e7681e699b6faf
PaulHaze <dev@artemist.com.au>
2022-01-15 07:46:23 +1100

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//* ADD THESE FOR REDUX
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
+
 import './styles/main.scss';
 import { Home } from './views';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
     <Home />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root'),
 );
